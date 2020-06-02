f572f640b6b0336c25fca47a6e92bcc8f4702fe9
Ester <ester.tataje@gmail.com>
2020-06-02 11:58:31 -0700

    add added simple effects and additional image of penguin
    co-authored-by: Sara Kane <kane.sarak@gmail.com>
    co-authored-by: Teresa Rosinski <trosinski89@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
   $(".clickable").click(function() {
-    $(".walrus-showing").toggle();
-    $(".walrus-hidden").toggle();
+    $("#walrus-showing").toggle();
+    $("#walrus-hidden").toggle();
+  });
+  $(".clickableFriends").click(function() {
+    $("#walrus-friends").show(1000);
+    //$("#walrus-friends").toggle();
   });
 });
